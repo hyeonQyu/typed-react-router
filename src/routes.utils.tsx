54a0e8776c40a73fd4bf702136591cd0aa2dd1da
hyeonQyu <dhk0561@naver.com>
2025-12-06 00:49:46 +0900

    feat: add custom hook for current route node access
    - Introduced `useCurrentRouteNode` hook to retrieve the current route node based on the pathname, utilizing the `getSafely` function for safe access to route data.

--- src/routes.utils.tsx
+++ src/routes.utils.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useContext } from 'react';
+import { getSafely } from './object.utils';
+import { PathValue } from './path.types';
 import { BaseMetadata, PartialRouteTree, RoutePathname, RouteTree } from './routes.types';
 
 export const createAppRoutes =
@@ -10,6 +12,11 @@ export const createAppRoutes =
       return useContext(Context);
     };
 
+    const useCurrentRouteNode = <TPath extends RoutePathname<TMetadata, TContext, TRouteTree>>(pathname: TPath) => {
+      const routes = useContext(Context);
+      return getSafely('/', routes, pathname) as PathValue<TRouteTree, TPath, '/'>;
+    };
+
     const AppRoutesProvider = ({ children }: { children: ReactNode }) => {
       return <Context.Provider value={appRoutes}>{children}</Context.Provider>;
     };
@@ -17,6 +24,7 @@ export const createAppRoutes =
     return {
       AppRoutesProvider,
       useAppRoutes,
+      useCurrentRouteNode,
       _types: {} as {
         AppRoutesPathname: RoutePathname<TMetadata, TContext, TRouteTree>;
       },
