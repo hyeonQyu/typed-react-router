d7e2b1e43b20db7f697a3e635b117ebd5a1bb751
hyeonQyu <dhk0561@naver.com>
2025-12-17 23:59:15 +0900

    feat: restructure project for monorepo support with Turbo

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   splitting: false,
   sourcemap: true,
   clean: true,
-  external: ["react", "react-dom", "react-router-dom"],
+  external: ["react"],
   treeshake: true,
   minify: false,
 });
+
